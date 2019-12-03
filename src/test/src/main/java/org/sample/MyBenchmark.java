d3f36f34e223026a1aba279c377f5600e937e8ab
Ilya V <ilyavy@users.noreply.github.com>
2019-12-03 18:59:40 +0300

    Update MyBenchmark.java

--- src/test/src/main/java/org/sample/MyBenchmark.java
+++ src/test/src/main/java/org/sample/MyBenchmark.java
@@ -1,44 +1,44 @@
-/*
- * Copyright (c) 2014, Oracle America, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- *  * Neither the name of Oracle nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.sample;
 
-import org.openjdk.jmh.annotations.Benchmark;
+import java.io.IOException;
+import java.util.List;
 
-public class MyBenchmark {
+import jila.parser.BookTextParser;
+import jila.reader.BookFileReader;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Setup;
+
+/*@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx2G"})*/
+public class BookAnalysisBenchmark {
+
+    private BookTextParser bookParser;
+
+    private List<String> sentences;
+
+    @Setup
+    public void setup() throws IOException {
+        bookParser = new BookTextParser();
+
+        // String text = BookFileReader.createInstance("little_red_riding_hood.txt").readIntoString();
+        String text = BookFileReader.createInstance("war-peace.txt").readIntoString();
+
+        sentences = bookParser.getSentences(text);
+        System.out.println("The number of sentences: " + sentences.size());
+        text = null;
+    }
 
     @Benchmark
-    public void testMethod() {
-        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
-        // Put your benchmark code here.
+    public void sequential() throws IOException {
+        bookParser.sequential(sentences);
     }
+
+    @Benchmark
+    public void forkJoinWithThreshold() throws IOException {
+        bookParser.forkJoinWithThreshold(sentences);
+    }
+
 
 }
