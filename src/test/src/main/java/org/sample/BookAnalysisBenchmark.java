c58f03af6599295238c38001a3a95ab8470f8e78
Ilya V <i.voloshanovskiy@innopolis.ru>
2019-12-03 23:18:23 +0300

    structure is changed

--- src/test/src/main/java/org/sample/BookAnalysisBenchmark.java
+++ src/test/src/main/java/org/sample/BookAnalysisBenchmark.java
@@ -2,16 +2,23 @@ package org.sample;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import jila.parser.BookTextParser;
 import jila.reader.BookFileReader;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 
-/*@BenchmarkMode(Mode.AverageTime)
+@BenchmarkMode(Mode.All)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
-@Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx2G"})*/
+@Fork(value = 1, jvmArgs = {"-Xms2G", "-Xmx2G"})
 public class BookAnalysisBenchmark {
 
     private BookTextParser bookParser;
@@ -40,5 +47,11 @@ public class BookAnalysisBenchmark {
         bookParser.forkJoinWithThreshold(sentences);
     }
 
+    public void futures() {
 
+    }
+
+    public void concurrentMapWithAtomics() {
+
+    }
 }
