5b8805e8038982bf46c84c78f620354e1b19405d
Ilya V <i.voloshanovskiy@innopolis.ru>
2020-01-14 08:54:54 +0300

    parallel stream additional implementations are added

--- src/main/java/jila/parser/ParallelStreamsBookTextParser.java
+++ src/main/java/jila/parser/ParallelStreamsBookTextParser.java
@@ -1,13 +1,9 @@
 package jila.parser;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -19,59 +15,35 @@ public class ParallelStreamsBookTextParser extends BookTextParser {
 
     @Override
     public Map<String, Word> countWords(List<String> sentences) {
-        /*long before = System.nanoTime();
-        Map<String, Word> wordsMap = sentences.parallelStream()
-                .map(sentence -> parseSentence(sentence, new HashMap<String, Word>()))
-                .reduce(this::mergeMaps)
-                .orElse(new HashMap<>());
-        long after = System.nanoTime();
-        long result = after - before;
-        System.out.println("time: " + result/1_000_000);*/
-
-        long before = System.nanoTime();
-        Map<String, Optional<Word>> wordsMap = sentences
-                .parallelStream()
-                .map(sentence -> {
-                    Pattern splitter = Pattern.compile(PATTERN);
-                    Matcher m = splitter.matcher(sentence);
-                    List<Word> list = new ArrayList<>();
-
-                    while (m.find()) {
-                        String wordStr = m.group().toLowerCase();
-                        if (wordStr.length() > WORD_LENGTH_THRESHOLD) {
-                            Word word = new SimpleWord(wordStr, sentence);
-                            word.incrementCount();
-                            list.add(word);
-                        }
-                    }
-                    return list;
-                })
+        Map<String, Word> wordsMap = sentences
+                .stream()
+                .map(this::parseSentence)
                 .flatMap(Collection::stream)
-                // .sorted() -> should not be used if groupingByConcurrent is used
-                // is collect is ok for parallel? (effective java)
-                // here it seems ok if concurrent alt is used:
-                // https://docs.oracle.com/javase/tutorial/collections/streams/parallelism.html
-                .collect(Collectors.groupingByConcurrent(Word::getWord, Collectors.reducing((w1, w2) -> {
-                    w1.incrementCount();
-                    return w1;
-                })));
+                .collect(Collectors.groupingByConcurrent(Word::getWord,
+                        Collectors.reducing(null, (w1, w2) -> {
+                            if (w1 == null) return w2;
+                            if (w2 == null) return w1;
 
-        long after = System.nanoTime();
-        long result = after - before;
-        System.out.println("time: " + result/1_000_000);
+                            w1.incrementCount();
+                            return w1;
+                        })));
 
-        System.out.println(wordsMap);
-
-        return null;
+        return wordsMap;
     }
 
-    private Map<String, Word> mergeMaps(Map<String, Word> left, Map<String, Word> right) {
-        left.forEach((k, v) ->
-                right.merge(k, v, (w1, w2) -> {
-                    w1.setCount(w1.getCount() + w2.getCount());
-                    return w1;
-                }));
+    private List<Word> parseSentence(final String sentence) {
+        Pattern splitter = Pattern.compile(PATTERN);
+        Matcher m = splitter.matcher(sentence);
+        List<Word> list = new ArrayList<>();
 
-        return right;
+        while (m.find()) {
+            String wordStr = m.group().toLowerCase();
+            if (wordStr.length() > WORD_LENGTH_THRESHOLD) {
+                Word word = new SimpleWord(wordStr, sentence);
+                word.incrementCount();
+                list.add(word);
+            }
+        }
+        return list;
     }
 }
