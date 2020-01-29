bd9679e14eea865659176d9e834ed8c12f721a61
Ilya V <i.voloshanovskiy@innopolis.ru>
2020-01-29 11:57:48 +0300

    Unit tests coverage analysis is added to CI

---
+++ src/test/java/jila/parser/BookTextParserTest.java
@@ -0,0 +1,30 @@
+package jila.parser;
+
+import org.junit.jupiter.api.Test;
+
+class BookTextParserTest {
+
+    @Test
+    void parseTextIntoSentences() {
+    }
+
+    @Test
+    void countWords() {
+    }
+
+    @Test
+    void getFleschScore() {
+    }
+
+    @Test
+    void countSyllables() {
+    }
+
+    @Test
+    void isVowel() {
+    }
+
+    @Test
+    void parseSentence() {
+    }
+}
