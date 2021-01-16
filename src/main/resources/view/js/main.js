da1febf23e727f0ca877a95cb87ca67c4a9ec45b
Ilia Voloshanovskii <ilia_voloshanovskii@neimanmarcus.com>
2021-01-15 17:56:37 -0800

    Refactor js code

--- src/main/resources/view/js/main.js
+++ src/main/resources/view/js/main.js
@@ -6,7 +6,7 @@ function analyzeLoading(status) {
     if (status === true) {
         document.getElementById("button_analyze");
     } else {
-        
+
     }
 }
 
@@ -48,18 +48,18 @@ function showWelcomeScreen() {
  * Check/Uncheck all words checkbox.
  */
 function allWords() {
-    var checkbox = document.getElementById("words_checkall");
-    var state = swapClass(checkbox, "words_checkbox_unchecked", "words_checkbox_checked");
-    
-    var oldState = "words_word_checkbox_unchecked";
-    var newState = "words_word_checkbox_checked";
+    const checkbox = document.getElementById("words_checkall");
+		const state = swapClass(checkbox, "words_checkbox_unchecked", "words_checkbox_checked");
+
+		let oldState = "words_word_checkbox_unchecked";
+		let newState = "words_word_checkbox_checked";
     if (state === "words_checkbox_unchecked") {
     	var temp = newState;
     	newState = oldState;
     	oldState = temp;
     }
-    
-    var boxes = document.getElementsByClassName("words_word_checkbox");
+
+		const boxes = document.getElementsByClassName("words_word_checkbox");
     for (var i = 0; i < boxes.length; i++) {
     	changeClass(boxes[i], oldState, newState);
     }
@@ -81,10 +81,10 @@ function selectWord(checkbox) {
  * @returns - installed class name
  */
 function swapClass(element, cName1, cName2) {
-	var classNames = element.className.split(" ");
-	var newClassName = classNames[0];
-	var iClassName = "";
-	
+	const classNames = element.className.split(" ");
+	let newClassName = classNames[0];
+	let iClassName = "";
+
 	for (var i = 1; i < classNames.length; i++) {
 		if (classNames[i] === cName1) {
 			classNames[i] = cName2;
@@ -93,26 +93,26 @@ function swapClass(element, cName1, cName2) {
 			classNames[i] = cName1;
 			iClassName = cName1;
 		}
-		
+
 		newClassName += (" " + classNames[i]);
 	}
-	
+
 	element.className = newClassName;
 	return iClassName;
 }
 
 
 function changeClass(element, oldName, newName) {
-	var classNames = element.className.split(" ");
-	var newClassName = classNames[0];
-	
+	const classNames = element.className.split(" ");
+	let newClassName = classNames[0];
+
 	for (var i = 1; i < classNames.length; i++) {
 		if (classNames[i] === oldName) {
 			classNames[i] = newName;
 		}
 		newClassName += (" " + classNames[i]);
 	}
-	
+
 	element.className = newClassName;
 }
 
@@ -127,12 +127,12 @@ function printEntitiesList(template, arr, curPage, amountOfPages) {
 	welcome_screen.style.display = "none";
 	words = document.getElementById("words");
 	words.style.display = "block";
-	
-	var text = "";
-    for(var i = 0; i < arr.length; i++) {
+
+	let text = "";
+    for(let i = 0; i < arr.length; i++) {
             obj = arr[i];
             if (obj != null) {
-            	var row = template;
+            	let row = template;
             	console.log(template);
             	row = row.replace(/%-id-%/g, "wc_" + obj.id);
             	row = row.replace(/%-frequency-%/g, obj.count);
@@ -143,34 +143,32 @@ function printEntitiesList(template, arr, curPage, amountOfPages) {
             }
     }
     document.getElementById("words_list").innerHTML = text;
-    
+
     curPager = document.getElementById("words_paginator_pages_page");
     curPager.value = curPage;
-    
+
     pages = document.getElementById("words_paginator_pages_amount");
     pages.innerHTML = amountOfPages;
 }
 
 
 function selectedWords() {
-	var boxes = document.getElementsByClassName("words_word_checkbox_checked");
-	var words = new Array(boxes.length);
-	
+	const boxes = document.getElementsByClassName("words_word_checkbox_checked");
+	const words = new Array(boxes.length);
+
 	for (var i = 0; i < boxes.length; i++) {
-		var boxHolder = boxes[i].parentNode;
-		var wordElement = boxHolder.parentNode;
-		var word = {
+		const boxHolder = boxes[i].parentNode;
+		const wordElement = boxHolder.parentNode;
+		const word = {
             id: boxes[i].id.replace("wc_", ""),
 			word: wordElement.children[2].innerHTML,
 			context: wordElement.children[3].innerHTML
 		}
 		words[i] = word;
-		
+
 		//swapClass(boxes[i], "words_word_checkbox_checked", "words_word_checkbox_unchecked");
 		//wordElement.style.display = "none";
 	}
 	console.log(JSON.stringify(words));
 	return JSON.stringify(words);
 }
-
-
